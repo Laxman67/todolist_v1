1d2ed4f45c41611fb5c1e9fc121d73f32ed82455
laksh <laxmanhenricks94@Gmail.com.com>
2023-03-12 16:25:40 +0530

    Commiting Before Adding Premade Css

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Global Variable
+
+let items = ["buy food", "cook food", "eat food"];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+// Send File
+app.get("/welcome", function (req, res) {
+  res.sendFile(__dirname + "/welcome.html");
+});
+
+
+
+app.get("/", function (req, res) {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kinOfdDay: day,
+    newListItems: items,
+  });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
+});
+
+app.listen("3000", function (req, res) {
+  console.log("Running on Port 3000");
+});
