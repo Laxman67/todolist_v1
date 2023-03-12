bc381df3fbb93a5bc4bcbc93838c31d8703dffd2
laksh <laxmanhenricks94@Gmail.com.com>
2023-03-12 22:56:51 +0530

    Able to add Layout using Ejs

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 //Global Variable
 
 let items = ["Buy Food", "Cook food", "Eat Food"];
-let workItems = [];
+let workItems = ["ok"];
 
 // Send File
 app.get("/welcome", function (req, res) {
@@ -39,7 +40,7 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
   let item = req.body.newItem;
 
-  if (req.body.newItem === "Work") {
+  if (req.body.list === "Work") {
     workItems.push[item];
     res.redirect("/work");
   } else {
@@ -51,7 +52,11 @@ app.post("/", function (req, res) {
 //  Work List
 
 app.get("/work", function (req, res) {
-  res.render("list", {ListTitle: "Laxman Work List",newListItems: workItems });
+  res.render("list", {
+    ListTitle: "Work",
+    newListItems: workItems,
+  });
+    
 });
 
 app.post("/work", function (req, res) {
@@ -60,6 +65,11 @@ app.post("/work", function (req, res) {
   res.redirect("/work");
 });
 
+
+app.get("/about", function (req,res) {
+  
+  res.render("about");
+})
 app.listen("3000", function (req, res) {
   console.log("Running on Port 3000");
 });
